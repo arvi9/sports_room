b6f7902c9cc9d35f27f60d4b8d8aea713424bba2
Athul Suresh <athulsure@gmail.com>
2016-10-23 18:18:29 +0530

    Change admin action list to buttons

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+* Postgres connectivity
+* Clean up codebase to include best practices from 2 scoops of Django
+* Remove SECRET_KEY and SMTP settings from settings.py before pushing to public_repo
+* Set up django signals/django-cron for periodic databse updates
--- sportsroom/equipment/admin.py
+++ sportsroom/equipment/admin.py
@@ -2,23 +2,37 @@ from django.contrib import admin
 from .models import Student, Equipment, BorrowedItem, Queue
 
 
-
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("username", "email", "phone_number")
 
+
 class EquipmentAdmin(admin.ModelAdmin):
-    list_display= ('category', 'n_equipment')
+    list_display = ('category', 'n_equipment')
+
+    actions = ['test',]
+
+    def paid(self, request, queryset):
+        queryset.update(n_equipment=0)
+        self.message_user(request, "Did shit successfully marked as published.")
+    test.short_description = "Test shit"
+
+    class Media:
+        js = ("js/action_buttons.js",)
+
+
 
 class BorrowedItemAdmin(admin.ModelAdmin):
     list_display = ('get_student', 'get_equipment', 'issue_date', 'due_date')
 
     def get_student(self, obj):
         return obj.student.name
+
     get_student.short_description = "Name"
 
     def get_equipment(self, obj):
         return obj.equipment.category
+
     get_equipment.short_description = "Category"
 
 
@@ -27,13 +41,15 @@ class QueueAdmin(admin.ModelAdmin):
 
     def get_student(self, obj):
         return obj.student.name
+
     get_student.short_description = "Name"
 
     def get_equipment(self, obj):
         return obj.equipment.category
+
     get_equipment.short_description = "Category"
+
 
 admin.site.register(Equipment, EquipmentAdmin)
 admin.site.register(BorrowedItem, BorrowedItemAdmin)
 admin.site.register(Queue, QueueAdmin)
-
